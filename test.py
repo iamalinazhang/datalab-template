97e464410ad2c317da1634cb859291f5923d4f32
iamalinazhang <hupo.zixuan.1203@gmail.com>
2024-10-02 21:35:19 +0800

    处理冲突时选择错了，fork后在pr对照手动处理冲突

--- test.py
+++ test.py
@@ -381,7 +381,7 @@ def main():
                     print(f"{' ' *33}error{i+1}: {msg_body}")
 
     max_points = sum(problem_info["rating"] for problem_info in problem_infos.values())
-    print(f"Total points: ....{total_points}/{max_points}.....")
+    print(f"Total points: {total_points}/{max_points}")
 
     # AutoGrader
     with open(".autograder_result", "w") as f:
